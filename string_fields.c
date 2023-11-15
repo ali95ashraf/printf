61344db8818ff775de1e8d761837c62c3f53ebac
ali95ashraf <aliashrafsaeed@gmail.com>
2023-11-15 12:48:04 -0800

    fix 2

--- string_fields.c
+++ string_fields.c
@@ -14,21 +14,17 @@ char *get_precision(char *p, params_t *parameters, va_list ap)
     if (*p != '.')
         return p;
 
-    p++; // Move to the character after '.'
+    p++;
     if (*p == '*')
     {
-        // If precision is specified as '*', get it from the argument list
         d = va_arg(ap, int);
-        p++; // Move to the next character
+        p++;
     }
     else
     {
-        // Read precision as a number
         while (_isdigit(*p))
             d = d * 10 + (*p++ - '0');
     }
-
-    // Update the precision in the parameters struct
     parameters->precision = d;
-    return (p); // Return the new pointer position in the format string
+    return (p);
 }
